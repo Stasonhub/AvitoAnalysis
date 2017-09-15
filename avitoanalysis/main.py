ca45d143bd512dc820dc8d02ebb95ce8758eb0f3
Smirnov Kirill <smirnov.k@yahoo.com>
2017-09-15 21:44:00 +0300

    Fixed

--- avitoanalysis/main.py
+++ avitoanalysis/main.py
@@ -3,17 +3,17 @@ import click
 from get_data import main as getter
 from settings import *
 
-
 @click.command()
-@click.option('--city', '-city', help='Name of needed city')
-@click.option('--type', '-t', help='What are we looking for?')
-@click.option('--category', '-cat', help='Number of category')
-@click.option('--search', '-s', help='Searching query')
+@click.option('--city', '-city')
+@click.option('--typ', '-t')
+@click.option('--category', '-cat')
+@click.option('--search', '-s')
 def main(city, typ, category, search):
     if not (city and typ and category and search):
         click.echo('You missed something')
 
-    url = 'https://www.avito.ru/{}/{}?s={}&q={}'.format(city, category, typ, search)
+    url = 'https://www.avito.ru/{}/{}?s={}&q={}'.format(city, typ, category, search)
+    click.echo(url)
     getter(url)
 
 if __name__ == '__main__':
